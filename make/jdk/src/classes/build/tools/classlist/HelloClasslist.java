d8b4269ea823e13874a98b6873d254cf2f52aa21
Andrew Leonard <anleonar@redhat.com>
2023-06-19 15:14:02 +0100

    classlist fix
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- make/jdk/src/classes/build/tools/classlist/HelloClasslist.java
+++ make/jdk/src/classes/build/tools/classlist/HelloClasslist.java
@@ -129,7 +129,7 @@ public class HelloClasslist {
 System.out.println("HELLOCLASSLIST: "+i);
 
         // Some archived cache classes not always loaded, ensure they are via these
-        String  fdbi = "double :" + (i / 17.0); // FDBigInteger: Use Prime to ensure significant digits
+        String  fdbi = "double :" + 5.6; // FDBigInteger: Use Prime to ensure significant digits
 System.out.println("HELLOCLASSLIST fdbi: "+fdbi);
         Byte    bc   = Byte.valueOf((byte)1);   // ByteCache
         Short   sc   = Short.valueOf((short)2); // ShortCache
